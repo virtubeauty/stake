fdb7ff1c59938b47a658e71c2184333384d2d944
onur-saf <48022642+onur-saf@users.noreply.github.com>
2025-05-09 01:50:48 +0300

    update wagmi config

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from '@wagmi/cli';
 import { react } from '@wagmi/cli/plugins';
 import { stakingContractAbi } from './src/abis/staking';
-import fs from 'fs';
-import dotenv from 'dotenv';
 import { erc20Abi } from 'viem';
-
-const network = process.env.NETWORK || 'testnet';
-const env = fs.readFileSync(`.env.${network}`);
-
-const config = dotenv.parse(env);
 
 export default defineConfig({
   out: 'src/wagmi/hooks.ts',
@@ -16,12 +9,13 @@ export default defineConfig({
     {
       name: 'StakingContract',
       abi: stakingContractAbi,
-      address: config.NEXT_PUBLIC_STAKING_CONTRACT_ADDRESS as `0x${string}`,
+      address: process.env
+        .NEXT_PUBLIC_STAKING_CONTRACT_ADDRESS as `0x${string}`,
     },
     {
       name: 'VBEA',
       abi: erc20Abi,
-      address: config.NEXT_PUBLIC_TOKEN_ADDRESS as `0x${string}`,
+      address: process.env.NEXT_PUBLIC_TOKEN_ADDRESS as `0x${string}`,
     },
   ],
   plugins: [react()],
